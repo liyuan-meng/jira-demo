0107784c504b8fba278b34b3d7faaf892e460ef5
liyuan-meng <13149260506@163.com>
2021-11-02 10:46:12 +0800

    feat: 添加登录表单

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css'
-import ProjectListScreen from "./screen/project-list";
+// import ProjectListScreen from "./screen/project-list";
+import LoginScreen from "./screen/login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen/>
+      <LoginScreen/>
     </div>
   );
 }
